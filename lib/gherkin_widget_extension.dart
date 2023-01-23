da04b44a86c8d79ff7095a98c0edeb9d07ab9d45
adrien_body <abody@oui.sncf>
2023-01-23 11:59:15 +0100

    add export

--- lib/gherkin_widget_extension.dart
+++ lib/gherkin_widget_extension.dart
@@ -8,3 +8,4 @@ export 'package:gherkin_widget_extension/reporters/custom_stdout_reporter.dart';
 export 'package:gherkin_widget_extension/reporters/monochrome_printer.dart';
 export 'package:gherkin_widget_extension/reporters/xml_reporter.dart';
 export 'package:gherkin_widget_extension/widget_test.dart';
+export 'package:gherkin_widget_extension/test_setup.dart';
