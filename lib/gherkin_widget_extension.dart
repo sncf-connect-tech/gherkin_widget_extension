d58d0d218d372e06164879fd88407c4c98e19dc4
adrien_body <abody@oui.sncf>
2023-01-23 10:39:28 +0100

    add export

--- lib/gherkin_widget_extension.dart
+++ lib/gherkin_widget_extension.dart
@@ -2,3 +2,4 @@ library gherkin_widget_extension;
 
 export 'package:gherkin_widget_extension/utils/json_loader.dart';
 export 'package:gherkin_widget_extension/world/cucumber_world.dart';
+export 'package:gherkin_widget_extension/world/widget_hooks.dart';
