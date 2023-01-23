6c7ba9797fd88f898773eeea395e47ab5af3143a
adrien_body <abody@oui.sncf>
2023-01-23 10:56:07 +0100

    add export

--- lib/gherkin_widget_extension.dart
+++ lib/gherkin_widget_extension.dart
@@ -4,3 +4,6 @@ export 'package:gherkin_widget_extension/utils/json_loader.dart';
 export 'package:gherkin_widget_extension/world/cucumber_world.dart';
 export 'package:gherkin_widget_extension/world/widget_hooks.dart';
 export 'package:gherkin_widget_extension/widget_objects/test_widgets.dart';
+export 'package:gherkin_widget_extension/reporters/custom_stdout_reporter.dart';
+export 'package:gherkin_widget_extension/reporters/monochrome_printer.dart';
+export 'package:gherkin_widget_extension/reporters/xml_reporter.dart';
