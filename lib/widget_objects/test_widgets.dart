430aa7c3c2fc3dca237b6190bfcd327c2870a5a0
Katia Chiron <katia_chiron@connect-tech.sncf>
2023-03-16 17:09:08 +0100

    Resolves #5 -  Defines the role of the TestWidgets abstract class

--- lib/widget_objects/test_widgets.dart
+++ lib/widget_objects/test_widgets.dart
@@ -1,1 +1,14 @@
-abstract class TestWidgets {}
+/// Each class inheriting from `TestWidgets` must:
+/// * be a private class
+/// * have a public variable to give access to methods of the private class
+/// The name of this variable must equals the class name (and follow dart naming conventions)
+///
+/// **Example:**
+/// ```
+/// final myWonderfulWidget = _MyWonderfulWidget();
+/// class _MyWonderfulWidget implements TestWidgets {}
+/// ```
+///
+abstract class TestWidgets {
+  Future<void> pumpItUp() async {}
+}
